baaf7da9a46908b5ad38a256eb1d6bbb56c96e87
Yash Singh <yashsinghofficial04@gmail.com>
2024-09-20 14:05:42 +0530

    signin api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,9 +1,20 @@
 import bcrypt from "bcrypt";
 import Student from "../model/student.model.js";
+import jwt from "jsonwebtoken";
 
 // Signup
 export const signup = async (req, res) => {
-  const { name, email, password, phone, skills, workingAt, yearOfPassing, course, batch } = req.body;
+  const {
+    name,
+    email,
+    password,
+    phone,
+    skills,
+    workingAt,
+    yearOfPassing,
+    course,
+    batch,
+  } = req.body;
 
   try {
     // Check if the email is already in use
@@ -39,5 +50,37 @@ export const signup = async (req, res) => {
   } catch (error) {
     console.error("Error during signup:", error);
     res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+// Signin
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const validStudent = await Student.findOne({ email });
+    if (!validStudent) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Student dont exist" });
+    }
+    // hasedd password matching
+    const isMatch = await bcrypt.compare(password, validStudent.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    //    Create a Jwt token
+    const token = jwt.sign({ _id:validStudent._id},process.env.JWT_SECRET)
+    const { password: pass , ...rest} = validStudent._doc;
+    res.cookie("access_token",token,{
+        httpOnly:true,
+        expires: new Date(Date.now()+ 24 * 60 * 60 * 1000)
+    })
+    .status(200)
+    .json(rest);
+  } catch (error) {
+    console.log(error);
   }
 };
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter, Link } from "react-router-dom";
-
+import { BrowserRouter , Routes , Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login"
+
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
-    </BrowserRouter>
+   <Navbar/>
+    <Routes>
+      <Route path="/sign-up" element={<Signup/>} />
+      <Route path="/sign-in" element={<Login/>} />
+    </Routes>
+   </BrowserRouter>
   );
 };
 
