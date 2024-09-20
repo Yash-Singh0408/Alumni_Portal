50f1432cf45d83be4e937cc00a21f347f2d53696
abhishekkhatale <abhishekkhatale007@gmail.com>
2024-09-20 13:38:25 +0530

    nav footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Link } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 const App = () => {
   return (
     <BrowserRouter>
     <Navbar/>
+    <Home/>
+    <Footer/>
     </BrowserRouter>
   );
 };
