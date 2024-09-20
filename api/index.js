2c29b4281f707bebfa19073906784cbfd74269a6
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2024-09-20 20:32:49 +0530

    backend- Admin model and controller added

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import connectDB from "./config/Database.js";
 import studentRouter from './router/student.router.js'
 import authRouter from './router/auth.router.js'
 import eventRouter from './router/Event.Router.js'
+import adminRouter from './router/admin.router.js'
 import cors from 'cors'
 
 const app = express();
@@ -13,6 +14,9 @@ app.use(cors({
     origin: 'http://localhost:5173', // Replace with your frontend URL
     credentials: true // This is necessary to allow cookies
   }));
+
+
+
 app.use(express.json());
 
 //Initialize Port Number
@@ -24,7 +28,8 @@ connectDB();
 //Routes
 app.use('/api/student',studentRouter)
 app.use('/api/auth',authRouter)
-app.use('/api/event',eventRouter)
+app.use('/api/admin',eventRouter)
+app.use('/api/admin',adminRouter)
 
 //Listining
 app.listen(PORT, () => console.log(`Server is Started On ${PORT}`));
