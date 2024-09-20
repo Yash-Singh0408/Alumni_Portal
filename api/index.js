a7778b2bd3efceb1de0c5d3e1c28a67120c0abb8
Yash Singh <yashsinghofficial04@gmail.com>
2024-09-20 12:27:31 +0530

    signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import connectDB from "./config/Database.js";
+import studentRouter from './router/student.router.js'
+import authRouter from './router/auth.router.js'
 
 const app = express();
 
@@ -12,6 +14,10 @@ const PORT = process.env.PORT || 3000;
 
 //Connect DB
 connectDB();
+
+//Routes
+app.use('/api/student',studentRouter)
+app.use('/api/auth',authRouter)
 
 //Listining
 app.listen(PORT, () => console.log(`Server is Started On ${PORT}`));
