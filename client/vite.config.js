0a003a6ff79c26884c35f3a433d5fde1e6d34ba7
Yash-Singh0408 <yashsinghofficial04@gmail.com>
2024-09-21 15:18:55 +0530

    changed vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Default is 'dist', but make sure this matches your Vercel setup
+  }
 })
